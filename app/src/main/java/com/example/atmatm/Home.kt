af807946a41ad4b70b837bd8173db83556f35e1d
Joaquin Aguilera <joaquinaguilera1@upb.edu>
2025-02-12 00:28:25 -0400

    Mejora a la interfaz y funciones implementadas

--- app/src/main/java/com/example/atmatm/Home.kt
+++ app/src/main/java/com/example/atmatm/Home.kt
@@ -3,11 +3,13 @@ package com.example.atmatm
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.example.atmatm.Deposito.Companion.ID_USER_DEPOSITO
 import com.example.atmatm.Retiro.Companion.ID_USER_RETIRO
 import com.example.atmatm.dataBase.AppDataBase
 import com.example.atmatm.dataBase.AppDataBase.Companion.getDatabase
 import com.example.atmatm.databinding.ActivityHomeBinding
+import kotlinx.coroutines.launch
 
 class Home : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
@@ -24,17 +26,39 @@ class Home : AppCompatActivity() {
 
         dbAccess = getDatabase(this)
 
-        binding.depositoCard.setOnClickListener {
-            val intentDeposito = Intent (this, Deposito::class.java)
-            intentDeposito.putExtra(ID_USER_DEPOSITO, intent.getStringExtra(ID_USER_HOME))
-            startActivity(intentDeposito)
-        }
+        cambiarARetiro()
+        cambiarADeposito()
+        setData()
 
+    }
+
+    private fun cambiarARetiro() {
         binding.retiroCard.setOnClickListener {
             val intentRetiro = Intent (this, Retiro::class.java)
             intentRetiro.putExtra(ID_USER_RETIRO, intent.getStringExtra(ID_USER_HOME))
             startActivity(intentRetiro)
         }
+    }
 
+    private fun cambiarADeposito() {
+        binding.depositoCard.setOnClickListener {
+            val intentDeposito = Intent (this, Deposito::class.java)
+            intentDeposito.putExtra(ID_USER_DEPOSITO, intent.getStringExtra(ID_USER_HOME))
+
+            startActivity(intentDeposito)
+        }
+    }
+
+    private fun setData() {
+        val idUser = intent.getStringExtra(ID_USER_DEPOSITO)
+        if (idUser != null) {
+            lifecycleScope.launch {
+                val user = dbAccess.userDao().getUserById(idUser)
+                val nombre = "${binding.holaHome.text} ${user.nombre}"
+                binding.holaHome.text = nombre
+                val saldo = "${user.saldo} Bs."
+                binding.saldo.text = saldo
+            }
+        }
     }
 }
