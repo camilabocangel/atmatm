692b78dbe56e2a8dd2237161e597c1121c00c1f8
Joaquin Aguilera <joaquinaguilera1@upb.edu>
2025-02-16 19:22:49 -0400

    pequeña mejora a las tarjetas

--- app/src/main/java/com/example/atmatm/Registro.kt
+++ app/src/main/java/com/example/atmatm/Registro.kt
@@ -63,7 +63,7 @@ class Registro : AppCompatActivity() {
                 email = email,
                 pin = pin.toInt(),
                 nombre = nombre,
-                numeroTarjeta = (0..9999999).random()
+                numeroTarjeta = (40000000..99999999).random()
             )
             lifecycleScope.launch {
                 dbAccess.userDao().insert(user)
